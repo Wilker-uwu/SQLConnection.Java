59c70c07a37ebf5417889c73729e403d5a9472aa
WilkerS1 <walkerxr@hotmail.com>
2018-08-23 20:48:45 -0300

    Progress with the interface building

--- src/main/java/com/wilkers1/sqlconnection/view/Insert.java
+++ src/main/java/com/wilkers1/sqlconnection/view/Insert.java
@@ -1,22 +1,55 @@
 package com.wilkers1.sqlconnection.view;
 
-import java.awt.Container;
 import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 import java.text.ParseException;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
 @SuppressWarnings("serial")
-public class Insert extends Window{
+public class Insert extends Window {
+	
+	private class Per extends JPanel {
+		/** Constraint arrays for setting up the fields. */
+		protected GridBagConstraints[][] field = null;
+		
+		protected JLabel lblNome = new JLabel("Nome:");
+		protected JLabel lblEmail = new JLabel("Email:");
+		
+		protected JFormattedTextField txtNome = null;
+		protected JFormattedTextField txtEmail = null;
+		
+		protected Per() throws ParseException {
+			super();
+			//layout setup
+			this.setLayout(bag);
+			this.field = Window.buildLayout(this.field, bag, Insert.PESSOA);
+			//formatted text field instances setup
+			txtNome = new JFormattedTextField(new MaskFormatter("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+			txtEmail = new JFormattedTextField(new MaskFormatter("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+			//title setup
+			this.add(new JLabel("Pessoa..."), this.field[0][0]);
+			//selector setup
+			this.add(lblEntity, this.field[1][0]);
+			this.add(cmbEntity, this.field[1][1]);
+			
+			//field setup
+			this.add(this.lblNome,  this.field[2][0]); //labels
+			this.add(this.lblEmail, this.field[2][0]);
+			this.add(this.txtNome,  this.field[3][1]); //fields
+			this.add(this.txtEmail, this.field[3][1]);
+		}
+	}
 	
 	private class Tar extends JPanel {
+		/** Constraint arrays for setting up the fields. */
 		protected GridBagConstraints[][] field = null;
 		
 		protected JLabel lblTitulo = new JLabel("Título:");
@@ -35,31 +68,75 @@ public class Insert extends Window{
 		protected JFormattedTextField txtPrazoInicio = new JFormattedTextField(mskData);
 		protected JFormattedTextField txtPrazoTermino = new JFormattedTextField(mskData);
 		
+		protected JComboBox<String> cmbMetodo = new JComboBox<String>(new String[] {"0. A", "1. B", "2. C", "?. ..."});
+		
 		protected Tar() throws ParseException {
 			super();
+			//layout setup
 			this.setLayout(bag);
-			this.field = Window.buildLayout(bag, Insert.TAREFA);
-			
+			this.field = Window.buildLayout(this.field, bag, Insert.TAREFA);
+			//formatted text field instances setup
 			txtTitulo = new JFormattedTextField(new MaskFormatter("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+			//title setup
+			this.add(new JLabel("Tarefas..."),this.field[0][0]);
+			//selector setup
+			this.add(lblEntity, this.field[1][0]);
+			this.add(cmbEntity, this.field[1][1]);
+			
+			//fields setup
+			this.add(lblTitulo,       this.field[2][0]); //labels
+			this.add(lblDesc,         this.field[3][0]);
+			this.add(lblPrazo,        this.field[4][0]);
+			this.add(lblPrazoInicio,  this.field[5][0]);
+			this.add(lblPrazoTermino, this.field[6][0]);
+			this.add(lblMetodo,       this.field[7][0]);
+			this.add(txtTitulo,       this.field[2][1]); //fields
+			this.add(scrDesc,         this.field[3][1]);
+			this.add(txtPrazo,        this.field[4][1]);
+			this.add(txtPrazoInicio,  this.field[5][1]);
+			this.add(txtPrazoTermino, this.field[6][1]);
+			this.add(cmbMetodo,       this.field[7][1]);
+			
 		}
 	}
 	
-	protected JLabel lblNome = new JLabel("Nome:");
-	protected JLabel lblEmail = new JLabel("Email:");
-	protected JTextField txtNome = new JTextField();
-	protected JTextField txtEmail = new JTextField();
+	/** Label for entity tyoe selector */
+	private JLabel lblEntity = new JLabel("Tipo de entidade:");
+	/** Entity selector combo box, for switching between panels */
+	private JComboBox<String> cmbEntity = new JComboBox<String>(new String[] {"Pessoa", "Tarefa", "Participantes"});
 	
-	private JPanel paneBtn = new JPanel();
-	private JButton btnInsert = new JButton();
+	/** JPanel instance for holding the action buttons */
+	private JPanel paneBtn = new JPanel(false);
+	/** Registration button for the database */
+	private JButton btnRegister = new JButton("Register");
+	/**  Clear button for clearing the text fields. */
+	private JButton btnClear = new JButton("Clear");
 	
-	public Insert(String windowName) {
+	/**
+	 * Constructor for the registration window.
+	 * @param windowName is the name of the window.
+	 * @throws ParseException 
+	 */
+	public Insert(String windowName) throws ParseException {
 		super(windowName);
-		this.setContentPane(new Tar());
+		
+		Per perPanel = new Per();
+		Tar tarPanel = new Tar();
+		
+		this.paneBtn.setLayout(new GridLayout(3,1));
+		this.paneBtn.add(this.btnRegister);
+		this.paneBtn.add(this.btnClear);
+		
+		this.setContentPane(perPanel);
 	}
 	
 	public static void main(String...args) {
 		//new Insert("Inserir", Window.TAREFA);
-		new Insert("Inserir");
+		try {
+			new Insert("Inserir");
+		} catch (ParseException exp) {
+			exp.printStackTrace();
+		}
 	}
 	
 }
--- src/main/java/com/wilkers1/sqlconnection/view/Window.java
+++ src/main/java/com/wilkers1/sqlconnection/view/Window.java
@@ -15,51 +15,88 @@ public class Window extends JFrame{
 	
 	protected Container pane = this.getContentPane();
 	
+	/** identifier for setting up {@link #buildLayout(GridBagLayout, int)} */
 	public static final int PESSOA = 0x20;
+	/** identifier for setting up {@link #buildLayout(GridBagLayout, int)} */
 	public static final int TAREFA = 0x70;
+	/** identifier for setting up {@link #buildLayout(GridBagLayout, int)} */
 	public static final int REF_PARTICIPANTE = Window.PESSOA; 
 	
+	/** variable for usage at {@link #buildLayout(GridBagLayout, int)} and other possible building methods */
 	private static int xgap = 4;
+	/** variable for usage at {@link #buildLayout(GridBagLayout, int)} and other possible building methods */
 	private static int ygap = 8;
+	/** variable for usage at {@link #buildLayout(GridBagLayout, int)} and other possible building methods */
 	private static int borderBoxSize = 40;
+	/** variable for usage at {@link #buildLayout(GridBagLayout, int)} and other possible building methods */
 	private static int lblx = 48;
+	/** variable for usage at {@link #buildLayout(GridBagLayout, int)} and other possible building methods */
 	private static int barx = 128;
+	/** variable for usage at {@link #buildLayout(GridBagLayout, int)} and other possible building methods */
 	private static int bary = 24;
 	
 	protected GridBagLayout bag = new GridBagLayout();
 	protected GridBagConstraints[][] field = null;
 	
+	/**
+	 * Basic setup constructor for the window.
+	 * @param windowName is the window title.
+	 */
 	public Window(String windowName) {
 		super(windowName);
 		this.setSize(480, 360);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.pane.setLayout(bag);
+		buildLayout(this.field, this.bag, PESSOA);
 	}
 	
 	/**
 	 * TODO complete window building
 	 * Prepares a layout according to the field layout specified.
+	 * 
+	 * <p>The first row holds a constraint for the title, while the last holds
+	 * another constraint for a new panel for the action buttons.</p>
+	 * 
+	 * <code>
+	 * [----TITLE----]</br>
+	 * [LABEL]-[FIELD]</br>
+	 * [LABEL]-[FIELD]</br>
+	 * [LABEL]-[FIELD]</br>
+	 * .</br>
+	 * .</br>
+	 * [BUTTON__PANEL]</br>
+	 * </code></br>
+	 * 
 	 * @param layout is the GridBagLayout used to prepare the spaces.
 	 * @param fieldSetup is the id of the field setup to insert.
 	 * @return
 	 */
-	public static GridBagConstraints[][] buildLayout(GridBagLayout layout, int fieldSetup) {
-		GridBagConstraints[][] pos = new GridBagConstraints[ //sets up the size of the layout
-															fieldSetup==PESSOA?4: //if PESSOA is selected
-															fieldSetup==TAREFA?8:2 //otherwise if TAREFA is selected, otherwise 2.
-															][0];
-		if(pos.length==2) { throw new IllegalArgumentException("Invalid setup."); }
+	public static GridBagConstraints[][] buildLayout(GridBagConstraints[][] pos, GridBagLayout layout, int fieldSetup) {
+		pos = new GridBagConstraints[ //sets up the size of the layout
+									 fieldSetup==PESSOA?5: //if PESSOA is selected
+									 fieldSetup==TAREFA?8:0 //otherwise if TAREFA is selected, otherwise 2.
+									 ][1];
+		if(pos.length==0) { throw new IllegalArgumentException("Invalid setup."); }
 		
-		for(GridBagConstraints[] posy : pos) { //repeats for every row of constraints...
-			posy = new GridBagConstraints[] { //set this position to be of two constraints
-					new GridBagConstraints(),
-					new GridBagConstraints(),
+		for(int row=1; row<pos.length; row++) {
+			pos[row] = new GridBagConstraints[] { //set this position to be of two constraints
+					new GridBagConstraints() {
+						{
+							this.fill = GridBagConstraints.BOTH;
+							this.gridx = 1;
+							this.gridy = row+2;
+						}
+					},
+					new GridBagConstraints() {
+						{
+							this.fill = GridBagConstraints.BOTH;
+							this.gridx = 3;
+							this.gridy = row+2;
+							this.gridwidth = 2;
+						}
+					},
 			};
-			for(GridBagConstraints posx : posy) { //repeats for every field...
-				posx.fill = GridBagConstraints.BOTH; //every field will will its entire constraint space.
-			}
-			posy[1].gridwidth = 2;
 		}
 		
 		//First and last rows...
